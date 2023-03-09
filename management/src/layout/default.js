3c5957e7ebbf8ff3fb3003b27420121484cea0a8
JackyZheng657 <j.zheng657@hotmail.com>
2023-03-09 18:17:56 +0100

    typscript to javascript again

--- management/src/layout/default.js
+++ management/src/layout/default.js
@@ -1,7 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./navbar";
 import Footer from "./footer";
-import React from "react"
 
 const DefaultLayout = () => {
     return (
--- management/src/modules/authorization/ProtectedRoute.js
+++ management/src/modules/authorization/ProtectedRoute.js
@@ -1,13 +1,8 @@
-import { FC } from 'react';
-import React from 'react';
 import { Navigate } from 'react-router-dom';
 import DefaultLayout from '../../layout/default';
 
-interface ProtectedRouteProps {
-    isAuthenticated: boolean;
-}
 
-const ProtectedRoute: FC<ProtectedRouteProps> = ({ isAuthenticated }) => {
+const ProtectedRoute = ({ isAuthenticated }) => {
     if (isAuthenticated) {
         return <Navigate to="/" />;
     }
