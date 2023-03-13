9b0a49bff8e8af09f060cbc7ec72cd45303f403c
JackyZheng657 <j.zheng657@hotmail.com>
2023-03-13 17:59:14 +0100

    request limiter added to all routes

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const { requestLimiter } = require('./middleware/security');
 const app = express();
 const mongodb = require('./services/database')();
 
 require('./models.js');
 require('./seed/dataSeeder.js');
 
-
 app.use('/', require('./routes'));
+app.use(requestLimiter);
+
 
 module.exports = app;
