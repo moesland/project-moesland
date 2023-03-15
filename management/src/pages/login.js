6dd18b6dd03c4e30aef4e00885df075737b204e4
JackyZheng657 <j.zheng657@hotmail.com>
2023-03-15 13:01:50 +0100

    removed use effect

--- management/src/pages/login.js
+++ management/src/pages/login.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { BackendClientRequest } from "../services/ApiClient";
 import { useNavigate } from "react-router-dom";
 
