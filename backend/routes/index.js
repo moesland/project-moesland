24b98999de9cc4930cb84fe6990aa37a614b0969
BjornVanBruchem <b.vanbruchem1@student.avans.nl>
2023-03-17 15:53:49 +0100

    Added authorisation to user routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { authenticateTokenRole } = require('../middleware/auth');
 const { requestLimiter } = require('../middleware/security');
 const router = express.Router();
 
@@ -12,9 +13,9 @@ router.use('/api/auth', requestLimiter, require('./api/auth'));
 router.use('/api/role', require('./api/role'));
 router.use('/api/user', require('./api/user'));
 
-router.use('/api/user/add', requestLimiter, require('./api/user/add'));
-router.use('/api/user/update', requestLimiter, require('./api/user/update'));
-router.use('/api/user/delete', requestLimiter, require('./api/user/delete'));
+router.use('/api/user/add', authenticateTokenRole("SuperAdmin"), requestLimiter, require('./api/user/add'));
+router.use('/api/user/update', authenticateTokenRole("SuperAdmin"), requestLimiter, require('./api/user/update'));
+router.use('/api/user/delete', authenticateTokenRole("SuperAdmin"), requestLimiter, require('./api/user/delete'));
 
 router.use('/api/authToken', require('./api/authToken'));
 router.use('/api/location', require('./api/location'));
