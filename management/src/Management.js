602a5a041132e81d9eeedbbf68aff1b7275efebe
FedorTselishchev <ikbenfedor@gmail.com>
2023-03-18 12:56:55 +0100

    inb

--- management/src/Management.js
+++ management/src/Management.js
@@ -39,21 +39,21 @@ export default function Management() {
     document.body.classList.remove('active-modal')
   }
 
-
+  var listOfManagers
 
   function addManager() {
 
-    var modalEmail = document.forms["modalAddManager"]["modalAddManagerEmailName"].value;
+    var modalEmail = document.forms["modalAddManager"]["modalAddManagerUserName"].value;
     var modalPassword = document.forms["modalAddManager"]["modalAddManagerPasswordName"].value;
 
-    var modalEmailError = document.getElementById("modalAddManagerEmailError")
+    var modalEmailError = document.getElementById("modalAddManagerUserError")
     var modalPasswordError = document.getElementById("modalAddManagerPasswordError")
-    
+
     modalPasswordError.classList.add("invisible")
     var errorCount = 0;
     //TODO meer condities, ffe navragen bij de boys
     if (modalEmail == "") {
-      modalEmailError.textContent = "Het email veld mag niet leeg zijn."
+      modalEmailError.textContent = "Het gebruikersnaam veld mag niet leeg zijn."
       modalEmailError.classList.remove("invisible")
       errorCount++
     }
@@ -75,21 +75,45 @@ export default function Management() {
     console.log("Deleteing manager")
 
   }
+  class Manager {
+    constructor(email, userName, password) {
+      this.email = email
+      this.userName = userName
+      this.password = password
+    }
+  }
+
+
+  function getListOfManagers()//faked
+  {
+    let mag = new Manager("Mark@gmail.com", "MarkoftheBeast", "123123a")
+    let mam = new Manager("Mark@gmail.com", "MarkoftheBeast", "123123a")
+    let pam = new Manager("Mark@gmail.com", "MarkoftheBeast", "123123a")
+
+    let listOfManagers = [];
+    listOfManagers = [mag, mam, pam]
+
+
+    var myTableBody = document.getElementById("tableBody")
+    myTableBody.removeAttribute("dummyTr")
+
+
+    for (var x = 0; x < listOfManagers.length; x++) {
+     
+      myTableBody.appendChild(document.getElementById("dummyTr").cloneNode(true))
+    }
+
+
+
+  }
+
 
   function editManager() {
 
     //var modalEmail = document.forms["modalEditManager"]["modalEditManagerEmailName"].value;
     //var modalPassword = document.forms["modalEditManager"]["modalEditManagerPasswordName"].value;
-    var modalEmail = document.getElementById("EditModalEmailInput").value
 
-    var modalEmailError = document.getElementById("EditModalEmailError")
-    var modalPasswordError = document.getElementById("EditModalPasswordError")
 
-    if (modalEmail == "") {
-      modalEmailError.value = "dit veld mag niet leeg zijn"
-      modalEmailError.classList.remove("invisible")
-    }
-    
 
     toggleEditModal()
 
@@ -100,38 +124,36 @@ export default function Management() {
     <>
       <div class="row">
         <div class="pt-5 col-md-8 mx-auto text-center">
-          <div class="float-end col-md-4 pb-3">
+          <div class="float-end col-md-3 pb-3">
             <button onClick={toggleModal} type="button" class="btn btn-success " data-toggle="modal" data-target="#myModal">
               Nieuwe Beheerder
             </button>
           </div>
-          <table class=" table table-borderless">
+          <table class=" table table-borderless" id="managerTable">
             <thead>
               <tr>
                 <th scope="col">#</th>
                 <th scope="col">Email</th>
+                <th scope="col">Gebruikersnaam</th>
                 <th scope="col">Wachtwoord</th>
                 <th scope="col">Rol</th>
                 <th scope="col">Acties</th>
               </tr>
             </thead>
-            <tbody>
-              <tr>
+            <tbody id="tableBody">
+              <tr id="dummyTr">
                 <th scope="row">1</th>
                 <td>Mark@gmail.com</td>
+                <td>MarkoftheBeast</td>
                 <td>123123a</td>
                 <td>Beheerder</td>
-                <td>
-                  <button onClick={deleteManager} class="btn btn-danger">Verwijderen</button>
-                  <button onClick={editManager} class="btn btn-success">Aanpassen</button>
-                </td>
-              </tr>
-              <tr>
-                <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>Beheerder</td>
-                <td>
+                <td >
+                  <div class="row">
+                    <div class="">
+                    </div>
+                    <div class="pr-3">
+                    </div>
+                  </div>
                   <button onClick={deleteManager} class="btn btn-danger">Verwijderen</button>
                   <button onClick={editManager} class="btn btn-success">Aanpassen</button>
                 </td>
@@ -152,9 +174,9 @@ export default function Management() {
               <form name="modalAddManager">
                 <div className="mx-auto  col-md-10">
                   <div class="form-group pt-3">
-                    <label for="exampleInputEmail1">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" name="modalAddManagerEmailName" aria-describedby="emailHelp" placeholder="Email"></input>
-                    <small id="modalAddManagerEmailError" class="form-text text-danger invisible">_GENERIC_ERROR_EMAIL</small>
+                    <label for="exampleInputEmail1">Gebruikersnaam</label>
+                    <input class="form-control" id="exampleInputEmail1" name="modalAddManagerUserName" aria-describedby="emailHelp" placeholder="Gebruikersnaam"></input>
+                    <small id="modalAddManagerUserError" class="form-text text-danger invisible">_GENERIC_ERROR_USER</small>
                   </div>
                   <div class="form-group pt-3">
                     <label for="exampleInputPassword1">Wachtwoord</label>
@@ -202,11 +224,11 @@ export default function Management() {
               <div class="modal-header">
                 <h5 class="modal-title">Beheerder aanpassen</h5>
               </div>
-              <form name="modalEditManager">
+              <form name="EditManagers">
                 <div className="mx-auto  col-md-10">
                   <div class="form-group pt-3">
                     <label for="exampleInputEmail1">Nieuwe Email address</label>
-                    <input type="email" class="form-control" id="EditModalEmailInput" name="modalEditManagerEmailName" aria-describedby="emailHelp" placeholder="Email"></input>
+                    <input type="email" class="form-control" id="EditModalEmailInput" name="EditManagersEmailName" aria-describedby="emailHelp" placeholder="Email"></input>
                     <small id="EditModalEmailError" class="form-text text-danger invisible"  >_GENERIC_ERROR_EMAIL</small>
                   </div>
                   <div class="form-group pt-3">
@@ -226,6 +248,7 @@ export default function Management() {
           </div>
         </div>
       )}
+      {window.onload = getListOfManagers}
     </>
   )
 }
