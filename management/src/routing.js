809a32d36defc96cdb231228fff6eab53d8eb291
Sven <sven.de.bresser@hotmail.com>
2023-03-22 16:38:39 +0100

    routing

--- management/src/routing.js
+++ management/src/routing.js
@@ -2,7 +2,6 @@ import { Routes, Route } from 'react-router-dom';
 import React from 'react';
 import Home from './pages/home';
 import Admin from './pages/admin';
-import ArticlesCreate from './pages/articles/create'
 import ProtectedRoute from './modules/authorization/ProtectedRoute';
 import DefaultLayout from './layout/default';
 import Login from './pages/login';
@@ -15,9 +14,7 @@ const Routing = () => {
         <Route element={<ProtectedRoute />}>
           <Route path="/admin" element={<Admin />} />
         </Route>
-        <Route element={<ProtectedRoute />}>
-          <Route path="/articles/create" element={<ArticlesCreate />} />
-        </Route>
+
         <Route element={<ProtectedRoute isAuthenticated={false}/>}>
           <Route path="/login" element={<Login />}/>
         </Route>
