359b5c72420ae5300b4503025c5ecaeb71b16b16
Sven <sven.de.bresser@hotmail.com>
2023-03-27 21:48:03 +0200

    code moved to repo

--- backend/repository/newsArticle.js
+++ backend/repository/newsArticle.js
@@ -8,5 +8,32 @@ module.exports = {
             .catch((err) => {
                 console.error(err);
             });
+    },
+    async createNewsArticle(title, date, bannerImage, content){
+        if(bannerImage != null){
+            const newArticle = new NewsArticle({
+                title: title,
+                content: content,
+                date: Date.now(),
+                bannerImage: bannerImage._id
+            });
+            await newArticle.save();
+        }
+        else{
+            const newImage = new Image({
+                name: bannerImage.originalname,
+                data: fs.readFileSync(filePath),
+                contentType: bannerImage.mimetype
+            });
+            const savedImage = await newImage.save();
+
+            const newArticle = new NewsArticle({
+                title: title,
+                content: content,
+                date: Date.now(),
+                bannerImage: savedImage._id
+            });
+            await newArticle.save();
+        }          
     }
 };
--- backend/routes/api/newsArticle/create.js
+++ backend/routes/api/newsArticle/create.js
@@ -4,17 +4,11 @@ const NewsArticle = require('../../../models/newsArticle');
 const Image = require('../../../models/image');
 const multer = require('multer');
 const upload = multer({ dest: 'uploads/' });
+const { createNewsArticle } = require('../../../repository/newsArticle');
 const fs = require('fs');
-
-// set up rate limiter: maximum of five requests per minute
-var RateLimit = require('express-rate-limit');
-var limiter = RateLimit({
-  windowMs: 1*60*1000, // 1 minute
-  max: 5
-});
+const path = require('path');
 
 router.use(express.json());
-router.use(limiter);
 
 router.get('/', async (req, res) => {
     res.send('Nieuwsartikel aanmaken');
@@ -22,41 +16,18 @@ router.get('/', async (req, res) => {
 
 router.post('/', upload.single('bannerImage'), async (req, res) => {
     try {
+        const {title, date, content } = req.body;
         const filePath = path.join(__dirname, '../../..', req.file.path);
-        const imageBuffer = fs.readFileSync(filePath);
+        const imageBuffer = fs.readFileSync(filePath); 
 
-        const existingImage = await Image.findOne({ 
+        const bannerImage = await Image.findOne({ 
             name: { $eq: req.file.originalname }, 
             data: { $eq: imageBuffer },
             contentType: { $eq: req.file.mimetype }
         });
 
-        if(existingImage){
-            const newArticle = new NewsArticle({
-                title: req.body.title,
-                content: req.body.content,
-                date: Date.now(),
-                bannerImage: existingImage._id
-            });
-            await newArticle.save();
-        }
-        else{
-            const newImage = new Image({
-                name: req.file.originalname,
-                data: fs.readFileSync(filePath),
-                contentType: req.file.mimetype
-            });
-            const savedImage = await newImage.save();
-
-            const newArticle = new NewsArticle({
-                title: req.body.title,
-                content: req.body.content,
-                date: Date.now(),
-                bannerImage: savedImage._id
-            });
-            await newArticle.save();
-        }          
-        res.status(201).send(`Newsarticle created`);
+        const newArticle = await createNewsArticle(title, date, bannerImage, content);
+         res.status(201).send(`News article created successfully!`);
     } catch (err) {
         res.status(500).send(`Could not create news article: ${err}`);
     }
