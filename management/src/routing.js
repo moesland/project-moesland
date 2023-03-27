0b972437ace0083129bc4b9768de5dec63395129
BjornVanBruchem <b.vanbruchem1@student.avans.nl>
2023-03-27 21:30:03 +0200

    Update view

--- management/src/routing.js
+++ management/src/routing.js
@@ -3,10 +3,12 @@ import React from 'react';
 import Home from './pages/home';
 import Admin from './pages/admin';
 import ArticlesCreate from './pages/articles/create'
+import ArticlesUpdate from './pages/articles/update'
 import ProtectedRoute from './modules/authorization/ProtectedRoute';
 import DefaultLayout from './layout/default';
 import Login from './pages/login';
 import ArticleOverview from './pages/articles/overview';
+import { defaultProps } from 'react-quill';
 
 const Routing = () => {
   return (
@@ -16,6 +18,7 @@ const Routing = () => {
         <Route element={<ProtectedRoute />}>
           <Route path="/admin" element={<Admin />} />
           <Route path="/articles/create" element={<ArticlesCreate />} />
+          <Route path="/articles/update/:id" element={<ArticlesUpdate />} />
           <Route path='/articles' element={<ArticleOverview/>} />
         </Route>
         <Route element={<ProtectedRoute isAuthenticated={false}/>}>
