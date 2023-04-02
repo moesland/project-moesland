6f08f96fb0b1d6506487ac0cc22455bd8c96d05c
BjornVanBruchem <b.vanbruchem1@student.avans.nl>
2023-04-02 11:39:01 +0200

    Creating image now works

--- backend/routes/api/userImage/create.js
+++ backend/routes/api/userImage/create.js
@@ -1,14 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const router = express.Router();
 const { createUserImage } = require('../../../repository/userImage');
+const Image = mongoose.model('Image');
 
 router.use(express.json());
 
 router.post('/', async (req, res) => {
     const { image } = req.body;
+    const { name, data, contentType } = image;
+
+    const newImage = new Image({
+        name, data, contentType
+    });
 
     try {
-        await createUserImage(image);
+        await createUserImage(newImage);
         res.status(200).send(`User image created successfully!`);
     } catch (err) {
         res.status(500).send(`Could not create user image: ${err}`);
