162362b66fca2c9488408128ae8f6521c9dcd57c
BjornVanBruchem <b.vanbruchem1@student.avans.nl>
2023-04-03 14:54:44 +0200

    Added authentication

--- backend/routes/api/userImage/index.js
+++ backend/routes/api/userImage/index.js
@@ -1,13 +1,14 @@
 const express = require('express');
+const { authenticateToken } = require('../../../middlewares/auth');
 
 const router = express.Router();
 
 router.use(express.json());
 
 router.use('/create', require('./create'));
-router.use('/approve', require('./approve'));
-router.use('/decline', require('./decline'));
-router.use('/restore', require('./restore'));
-router.use('/delete', require('./delete'));
+router.use('/approve', authenticateToken, require('./approve'));
+router.use('/decline', authenticateToken, require('./decline'));
+router.use('/restore', authenticateToken, require('./restore'));
+router.use('/delete', authenticateToken, require('./delete'));
 
 module.exports = router;
