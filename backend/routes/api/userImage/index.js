b3b68a4927b1d38762a51fcb2fbead86f9164395
BjornVanBruchem <b.vanbruchem1@student.avans.nl>
2023-04-04 17:15:35 +0200

    Authentication

--- backend/routes/api/userImage/index.js
+++ backend/routes/api/userImage/index.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.use(express.json());
 
-router.get('/', async (req, res) => {
+router.get('/', authenticateToken, async (req, res) => {
     try {
         res.status(200).json(await getAllUserImages());
     } catch (err) {
