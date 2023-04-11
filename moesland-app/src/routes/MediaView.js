8abe569ceec659d8b396a09ced90c62322f75e82
JackyZheng657 <j.zheng657@hotmail.com>
2023-04-11 21:09:36 +0200

    finished

--- moesland-app/src/routes/MediaView.js
+++ moesland-app/src/routes/MediaView.js
@@ -1,25 +1,31 @@
 import axios from 'axios';
 import React, { Component, useEffect, useState } from 'react';
-import { Text, View } from 'react-native';
+import { Alert, Text, View } from 'react-native';
 import { Button } from 'react-native-elements';
-import {BACKEND_URL } from "@env"
+import { BACKEND_URL } from "@env"
 
 export default MediaView = () => {
     const [selectedImage, setSelectedImage] = useState(null);
 
     useEffect(() => {
         setSelectedImage({
-            name: "test name", 
-            data: "test data", 
+            name: "test name",
+            data: "test data",
             contentType: "test content"
-        }) 
+        })
     }, [])
 
     const uploadImage = async () => {
         const url = `${BACKEND_URL}/api/user-image/create`
+        axios.post(url, selectedImage).then(response => {
+            if (response.status == 200) {
+                cancelImage();
+                Alert.alert("Melding", `${selectedImage.name} is upgeload!`)
+            }
+        }).catch(error => {
+            console.log(error);
+        })
 
-        axios.post(url, selectedImage);
-        
     }
 
     const cancelImage = () => {
@@ -30,11 +36,11 @@ export default MediaView = () => {
         <View>
             <Text>Media</Text>
 
-            { selectedImage &&
-            <View>
-                <Button onPress={uploadImage} title="Upload" />
-                <Button onPress={cancelImage} title="Annuleren" />
-            </View>
+            {selectedImage &&
+                <View>
+                    <Button onPress={uploadImage} title="Upload" />
+                    <Button onPress={cancelImage} title="Annuleren" />
+                </View>
             }
         </View>
     )
