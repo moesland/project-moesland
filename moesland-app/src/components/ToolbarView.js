6a3312a108a7f6f13d5f9fcd8ab1dbf93d7f37be
xpetersen <ptrsnxndr@gmail.com>
2023-04-11 11:42:08 +0200

    made code more readable

--- moesland-app/src/components/ToolbarView.js
+++ moesland-app/src/components/ToolbarView.js
@@ -5,24 +5,27 @@ import { MOESLAND_GREEN, COLOR_WHITE } from '../constants/colors';
 
 const ToolbarView = ({ showBackButton, onPressMenu }) => {
   const navigation = useNavigation();
+
+  const menuIcon = (
+    <Icon
+      name="menu"
+      onPress={onPressMenu}
+      color={COLOR_WHITE}
+    />
+  );
+
+  const backButton = (
+    <Icon
+      name="arrow-back"
+      onPress={() => navigation.goBack()}
+      color={COLOR_WHITE}
+    />
+  );
+
   return (
     <Header
       backgroundColor={MOESLAND_GREEN}
-      leftComponent={
-        showBackButton ? (
-          <Icon
-            name="arrow-back"
-            onPress={() => navigation.goBack()}
-            color={COLOR_WHITE}
-          />
-        ) : (
-          <Icon
-            name="menu"
-            onPress={onPressMenu}
-            color={COLOR_WHITE}
-          />
-        )
-      }
+      leftComponent={showBackButton ? backButton : menuIcon}
     />
   );
 };
