7842d4adbc56497351087299e0679c0285c22ea8
Sven <sven.de.bresser@hotmail.com>
2023-04-17 15:47:14 +0200

    Implementation and seeder

---
+++ backend/seeds/eventSeeder.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const Events = mongoose.model('Event');
+
+const eventData = [
+  {
+    title: 'Pronkzitting',
+    description: 'Een jaarlijks terugkerend avondvullend programma waar het publiek door Schaijkse rasartiesten vermaakt wordt. De artiesten zijn Schaijkse mensen die met eigen teksten of eigen liedjes Schaijkse gebeurtenissen op de korrel nemen en het publiek weten te boeien. De voorbereidingen nemen lang van te voren hun aanvang, zo ongeveer bij de opening van het jaarlijkse carnaval, zijnde het 11-11 bal. Op de eerste bijeenkomst met artiesten en commissie wordt de opzet doorgesproken en gekeken wie er mee wil doen en wat voor ideeën er zijn.',
+    startdate: Date(),
+    enddate: Date(),
+    location: 'Schaijk'
+  },
+  {
+    title: 'Erewijn',
+    description: 'Zaterdags voorafgaand aan de carnavalsdagen organiseert de gemeente Landerd de zgn. Erewijn. Tijdens de Erewijn krijgen de hoogheden van Schaijk, Zeeland en Reek officieel de sleutel van de diverse rijken overhandigd.',
+    startdate: Date(),
+    enddate: Date(),
+    location: 'Schaijk'
+  },
+];
+
+const seedEvents = async () => {
+  await Events.find({}).then(async (events) => {
+    if (!events.length) {
+      console.log('\tNo events found, filling seed data');
+      await Events.insertMany(eventData)
+        .then(() => console.log('\tFilling events seed data succesfull'))
+        .catch((err) => console.log('\tFilling events seed data failed', err));
+    }
+  });
+};
+
+module.exports = async () => {
+  await seedEvents();
+};
