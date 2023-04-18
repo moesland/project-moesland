194d9baa356ad796ee48f28fe69e972cd5b2120f
BjornVanBruchem <b.vanbruchem1@student.avans.nl>
2023-04-18 17:02:09 +0200

    A bit of work

--- management/src/pages/articles/create.js
+++ management/src/pages/articles/create.js
@@ -1,14 +1,20 @@
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import React, { useState, useRef } from "react";
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 import { useNavigate } from "react-router-dom";
 
-const Create = () => {
-    const [editorHtml, setEditorHtml] = useState("");
+export default function Create() {
     const [title, setTitle] = useState('');
     const [bannerImage, setBannerImage] = useState('');
     const quillRef = useRef(null);
-  
+
+    const schema = yup.object().shape({
+        title: yup.string().min(3, "De titel moet minimaal drie karakters bevatten.").required("Dit veld mag niet leeg zijn.")
+    });
+
     const navigate = useNavigate();
 
     const modules = {
@@ -33,17 +39,17 @@ const Create = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        
+
         const delta = quillRef.current.getEditor().getContents();
         const content = JSON.stringify(delta);
 
         const imageFile = document.querySelector('input[name="image"]').files[0];
-    
+
         const formData = new FormData();
         formData.append('title', title);
         formData.append('content', content);
         formData.append('bannerImage', imageFile);
-    
+
         const token = localStorage.getItem('token');
 
         const response = await fetch('http://localhost:5000/api/news-article/create', {
@@ -53,7 +59,7 @@ const Create = () => {
                 Authorization: `Bearer ${token}`,
             },
         });
-    
+
         if (response.ok) {
             window.alert('Nieuwsartikel is aangemaakt!');
             navigate('/articles/');
@@ -76,25 +82,25 @@ const Create = () => {
                             <label className="mb-2">
                                 Titel:
                             </label>
-                            <input type="text" name="title" className="form-control" value={title} onChange={(e) => setTitle(e.target.value)} required/>
+                            <input type="text" name="title" className="form-control" value={title} onChange={(e) => setTitle(e.target.value)} required />
                         </div>
                         <div className="form-group mt-3">
                             <label className="mb-2">
                                 Afbeelding:
                             </label>
-                            <input type="file" name="image" accept="image/*" className="form-control" onChange={handleImageChange} required/>
+                            <input type="file" name="image" accept="image/*" className="form-control" onChange={handleImageChange} required />
                         </div>
                         <div className="form-group mt-3">
                             <label className="mb-2">
                                 Inhoud:
                             </label>
-                            <ReactQuill ref={quillRef} name="conent" modules={modules}/>
+                            <ReactQuill ref={quillRef} name="conent" modules={modules} />
                         </div>
-                        <br></br>   
+                        <br></br>
                         <div className="form-group text-left">
                             <div className="row">
                                 <div className="col text-start">
-                                    <input type="submit" value="Aanmaken" className="btn btn-success w-50"/>
+                                    <input type="submit" value="Aanmaken" className="btn btn-success w-50" />
                                 </div>
                                 <div className="col text-end">
                                     <a href="/articles/" className="btn btn-danger w-50">Annuleren</a>
@@ -105,7 +111,5 @@ const Create = () => {
                 </div>
             </div>
         </>
-    )
-    }
-    
-export default Create;
+    );
+}
