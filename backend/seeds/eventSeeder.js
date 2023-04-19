55e12832a4c0957d3479cab81fc7672e14b5465f
Sven <sven.de.bresser@hotmail.com>
2023-04-19 14:48:23 +0200

    update seeder

--- backend/seeds/eventSeeder.js
+++ backend/seeds/eventSeeder.js
@@ -1,20 +1,27 @@
 const mongoose = require('mongoose');
-
 const Events = mongoose.model('Event');
+
+const today = new Date();
+const tomorrow = new Date(today);
+tomorrow.setDate(tomorrow.getDate() + 1);
+const dayAfterTomorrow = new Date(today);
+dayAfterTomorrow.setDate(dayAfterTomorrow.getDate() + 2);
+const dayAfterTheDayAfterTomorrow = new Date(today);
+dayAfterTheDayAfterTomorrow.setDate(dayAfterTheDayAfterTomorrow.getDate() + 3);
 
 const eventData = [
   {
     title: 'Pronkzitting',
     description: 'Een jaarlijks terugkerend avondvullend programma waar het publiek door Schaijkse rasartiesten vermaakt wordt. De artiesten zijn Schaijkse mensen die met eigen teksten of eigen liedjes Schaijkse gebeurtenissen op de korrel nemen en het publiek weten te boeien. De voorbereidingen nemen lang van te voren hun aanvang, zo ongeveer bij de opening van het jaarlijkse carnaval, zijnde het 11-11 bal. Op de eerste bijeenkomst met artiesten en commissie wordt de opzet doorgesproken en gekeken wie er mee wil doen en wat voor ideeën er zijn.',
-    startdate: Date(),
-    enddate: Date(),
+    startdate: today,
+    enddate: tomorrow,
     location: 'Schaijk'
   },
   {
     title: 'Erewijn',
     description: 'Zaterdags voorafgaand aan de carnavalsdagen organiseert de gemeente Landerd de zgn. Erewijn. Tijdens de Erewijn krijgen de hoogheden van Schaijk, Zeeland en Reek officieel de sleutel van de diverse rijken overhandigd.',
-    startdate: Date(),
-    enddate: Date(),
+    startdate: dayAfterTomorrow,
+    enddate: dayAfterTheDayAfterTomorrow,
     location: 'Schaijk'
   },
 ];
