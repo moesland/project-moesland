44233125ef43e62e62254e81d3d1ace70ca849c6
BjornVanBruchem <b.vanbruchem1@student.avans.nl>
2023-04-20 22:07:23 +0200

    Hopefully fix filePath injection stuff

--- backend/routes/api/newsArticle/create.js
+++ backend/routes/api/newsArticle/create.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const NewsArticle = require('../../../models/newsArticle');
 const Image = require('../../../models/image');
 const multer = require('multer');
 const upload = multer({ dest: 'uploads/', limits: { fieldSize: 50 * 1024 * 1024 } });
@@ -11,15 +10,15 @@ const auth = require('../../../middlewares/auth');
 
 router.use(express.json());
 
-router.get('/', async (req, res) => {
-    res.send('Nieuwsartikel aanmaken');
-});
-
 router.post('/', auth.authenticateToken, upload.single('bannerImage'), async (req, res) => {
-   try {
-        const {title, date, content } = req.body;
+    try {
+        const { title, date, content } = req.body;
         const filePath = path.join(__dirname, '../../..', req.file.path);
-        const imageBuffer = fs.readFileSync(filePath); 
+        if (!fs.existsSync(filePath)) {
+            return res.status(500).send(`Could not create news article: file does not exist, path: ${filePath}.`);
+        }
+
+        const imageBuffer = fs.readFileSync(filePath);
 
         const bannerImage = new Image({
             name: req.file.originalname,
@@ -27,8 +26,8 @@ router.post('/', auth.authenticateToken, upload.single('bannerImage'), async (re
             contentType: req.file.mimetype
         });
 
-        const newArticle = await createNewsArticle(title, date, bannerImage, content);
-         res.status(201).send(`News article created successfully!`);
+        await createNewsArticle(title, date, bannerImage, content);
+        res.status(201).send(`News article created successfully!`);
     } catch (err) {
         res.status(500).send(`Could not create news article: ${err}`);
     }
