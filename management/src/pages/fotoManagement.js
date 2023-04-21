9d02ed768ed9fe975b913c408acda6fab5c1618e
FedorTselishchev <ikbenfedor@gmail.com>
2023-04-21 11:11:05 +0200

    minor edit

--- management/src/pages/fotoManagement.js
+++ management/src/pages/fotoManagement.js
@@ -1,32 +1,99 @@
 import React, { useEffect, useState } from "react";
-
+import ImageGallery from 'react-image-gallery';
 
 export default function Management() {
+    const images = [
+        {
+          original: "https://live.staticflickr.com/65535/52771907979_964013db83_k.jpg",
+          thumbnail: "https://live.staticflickr.com/65535/52771907979_964013db83_k.jpg",
+          description: '1'
+      
+        },
+        {
+          original: "https://live.staticflickr.com/65535/52771650596_507e8a54d2_k.jpg",
+          thumbnail: "https://live.staticflickr.com/65535/52771650596_507e8a54d2_k.jpg",
+          description: '2'
+      
+        },
+        {
+          original: "https://live.staticflickr.com/65535/52771648561_1121158507_k.jpg",
+          thumbnail: "https://live.staticflickr.com/65535/52771648561_1121158507_k.jpg",
+          description: '3'
+      
+        },
+        {
+          original: "https://live.staticflickr.com/65535/52772136488_f4661a549f_k.jpg",
+          thumbnail: "https://live.staticflickr.com/65535/52772136488_f4661a549f_k.jpg",
+          description: '4'
+      
+        }
+      ];
+
+    const [galleryImages, setGalleryImages] = useState(images);
+
+    useEffect(() => {
+        setGalleryImages(images);
+    }, [images]);
+
+    const handleImageClick = (event) => {
+        const description = event.target.dataset.description;
+        console.log(description)
+        const index = images.findIndex((item) => item.description === description);
+        const newImages = images.filter((item, i) => i !== index);//wrm werkt niet??
+        setGalleryImages(newImages);
+    };
+
+    const handleSwipe = (swipeDirection) => {
+        if (swipeDirection === 'left') {
+            // handle swipe left
+            console.log("denied")
+        } else if (swipeDirection === 'right') {
+            // handle swipe right
+            console.log("approved")
+        }
+    };
+
+    const approveItem = (item) => {
+        console.log(`Item approved: ${item.description}`);
+    };
+
+    const denyItem = (item) => {
+        console.log(`Item denied: ${item.description}`);
+    };
+
+    const renderItem = (item) => {
+        return (
+            <>
+                <div className="card">
+                    <img className="image-gallery-image"
+                        src={item.original}
+                        alt={item.description}
+                        data-description={item.description}
+                    />
+                </div>
+                <button
+                    type="button"
+                    onClick={denyItem(item)}
+                    className="btn btn-danger"
+                >
+                    Afkeuren
+                </button>
+                <button
+                    type="button"
+                    className="btn btn-success"
+                    onClick={approveItem(item)}
+                >
+                    Goedkeuren
+                </button>
+            </>
+        );
+    };
+
     return (
         <>
-
-            <div class="row">
-                <div class="column">
-                    <img class="thumbnale" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Ayinger_Br%C3%A4uweisse_at_restaurant_Jano.jpg/1280px-Ayinger_Br%C3%A4uweisse_at_restaurant_Jano.jpg" />
-                    <img class="thumbnale" src="https://images0.persgroep.net/rcs/2CY0m8gAlkZALGfuE7z9Z7ODqoc/diocontent/176494880/_fitwidth/763?appId=93a17a8fd81db0de025c8abd1cca1279&quality=0.8&desiredformat=webp" />
-                    <img class="thumbnale" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Ayinger_Br%C3%A4uweisse_at_restaurant_Jano.jpg/1280px-Ayinger_Br%C3%A4uweisse_at_restaurant_Jano.jpg" />
-                </div>
-                <div class="column">
-                    <img class="thumbnale" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Ayinger_Br%C3%A4uweisse_at_restaurant_Jano.jpg/1280px-Ayinger_Br%C3%A4uweisse_at_restaurant_Jano.jpg" />
-                    <img class="thumbnale" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Ayinger_Br%C3%A4uweisse_at_restaurant_Jano.jpg/1280px-Ayinger_Br%C3%A4uweisse_at_restaurant_Jano.jpg" />
-                    <img class="thumbnale" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Ayinger_Br%C3%A4uweisse_at_restaurant_Jano.jpg/1280px-Ayinger_Br%C3%A4uweisse_at_restaurant_Jano.jpg" />
-                    <img class="thumbnale" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Ayinger_Br%C3%A4uweisse_at_restaurant_Jano.jpg/1280px-Ayinger_Br%C3%A4uweisse_at_restaurant_Jano.jpg" />
-
-                </div>
-                <div class="column">
-                    <img class="thumbnale" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Ayinger_Br%C3%A4uweisse_at_restaurant_Jano.jpg/1280px-Ayinger_Br%C3%A4uweisse_at_restaurant_Jano.jpg" />
-                    <img class="thumbnale" src="https://images0.persgroep.net/rcs/2CY0m8gAlkZALGfuE7z9Z7ODqoc/diocontent/176494880/_fitwidth/763?appId=93a17a8fd81db0de025c8abd1cca1279&quality=0.8&desiredformat=webp" />
-                    <img class="thumbnale" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Ayinger_Br%C3%A4uweisse_at_restaurant_Jano.jpg/1280px-Ayinger_Br%C3%A4uweisse_at_restaurant_Jano.jpg" />
-                </div>
-                <div class="column">
-                    <img class="thumbnale" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Ayinger_Br%C3%A4uweisse_at_restaurant_Jano.jpg/1280px-Ayinger_Br%C3%A4uweisse_at_restaurant_Jano.jpg" />
-                    <img class="thumbnale" src="https://images0.persgroep.net/rcs/2CY0m8gAlkZALGfuE7z9Z7ODqoc/diocontent/176494880/_fitwidth/763?appId=93a17a8fd81db0de025c8abd1cca1279&quality=0.8&desiredformat=webp" />
-                    <img class="thumbnale" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Ayinger_Br%C3%A4uweisse_at_restaurant_Jano.jpg/1280px-Ayinger_Br%C3%A4uweisse_at_restaurant_Jano.jpg" />
+            <div className="app">
+                <div className="image-gallery-wrapper">
+                    <ImageGallery items={galleryImages} onClick={handleImageClick} onSwipe={handleSwipe} renderItem={renderItem} />
                 </div>
             </div>
         </>
