ebed529b2c33f4965f5663ba7f0ee175736ea2dc
xpetersen <ptrsnxndr@gmail.com>
2023-04-25 13:08:41 +0200

    created calendar view

--- management/src/pages/events/overview.js
+++ management/src/pages/events/overview.js
@@ -1,21 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React, {useState} from 'react';
+import Calendar from 'react-calendar';
 import { useNavigate } from "react-router-dom";
-import { BackendClientRequest } from "../../services/ApiClient";
+import "./overview.css"
 
 const EventOverview = () => {
-    const [events, setEvents] = useState(undefined);
+
+    const [date, setDate] = useState(new Date());
 
     const navigate = useNavigate();
-
-    useEffect(() => {
-        // const fetchEventData = async () => {
-        //     await fetch(process.env.REACT_APP_BACKEND_ROOT_URL + "/api/events/", { method: "GET" })
-        //         .then(response => response.json())
-        //         .then(data => { setEvents(data)});
-        // }
-
-        // fetchEventData()
-    }, [])
 
 
     const openCreate = () => {
@@ -23,12 +15,30 @@ const EventOverview = () => {
     }
 
     return (
-        <div className="container mt-3">
-            <div className="mb-3">
-                <button className="btn btn-moesland" onClick={openCreate}> Nieuw evenement</button>
-            </div>
+        <div className='app'>
+          <h1 className='text-center'>React Calendar with Range</h1>
+          <div className='calendar-container'>
+            <Calendar
+              onChange={setDate}
+              value={date}
+              selectRange={true}
+            />
+          </div>
+          {date.length > 0 ? (
+            <p className='text-center'>
+              <span className='bold'>Start:</span>{' '}
+              {date[0].toDateString()}
+              &nbsp;|&nbsp;
+              <span className='bold'>End:</span> {date[1].toDateString()}
+            </p>
+          ) : (
+            <p className='text-center'>
+              <span className='bold'>Default selected date:</span>{' '}
+              {date.toDateString()}
+            </p>
+          )}
         </div>
-    )
-}
+      );
+    }
 
 export default EventOverview;
