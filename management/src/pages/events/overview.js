08189f30fd3d6052ec40596ec9254c8ec4d03794
xpetersen <ptrsnxndr@gmail.com>
2023-05-08 20:26:09 +0200

    added list

--- management/src/pages/events/overview.js
+++ management/src/pages/events/overview.js
@@ -3,11 +3,11 @@ import Calendar from 'react-calendar';
 import { useNavigate } from "react-router-dom";
 import "./overview.css"
 import ModalAdd from "../../modules/events/modalAdd";
-// import ModalDelete from "../modules/events/modalDelete";
-// import ModalUpdate from "../modules/events/modalUpdate";
+import ModalDelete from "../../modules/events/modalDelete";
+import ModalUpdate from "../../modules/events/modalUpdate";
 
 const EventOverview = () => {
-
+    const userData = []
     const [date, setDate] = useState(new Date());
     const [ShowAddEventModal, setShowAddEventModal] = useState(false);
     const navigate = useNavigate();
@@ -17,7 +17,7 @@ const EventOverview = () => {
     }
 
     const createEvent = () => {
-        ToggleShowAddEventModal();
+        ToggleShowAddEventModal(date);
     }
 
     return (
@@ -27,7 +27,6 @@ const EventOverview = () => {
             <div className='d-flex justify-content-center'>
                 <Calendar
                     onChange={setDate}
-                    onClickDay={createEvent}
                     value={date}
                     selectRange={true}
                 />
@@ -41,11 +40,57 @@ const EventOverview = () => {
                 </p>
             ) : (
                 <p className='text-center'>
-                    <span className='bold'>Default selected date:</span>{' '}
                     {date.toDateString()}
                 </p>
             )}
-            {ShowAddEventModal && <ModalAdd toggleModal={ToggleShowAddEventModal} />}
+
+            {true &&
+                <div className="row">
+                    <div className="pt-5 col-md-8 mx-auto text-center">
+                        <div className="float-end col-md-3 pb-3">
+                            <button onClick={createEvent} type="button" className="btn btn-moesland">
+                                Nieuw Evenement
+                            </button>
+                        </div>
+                        <table className=" table table-striped " >
+                            <thead>
+                                <tr className="bg-moesland text-white">
+                                    <th scope="col">Datum</th>
+                                    <th scope="col">Titel</th>
+                                    <th scope="col">Locatie</th>
+                                    <th scope="col">Acties</th>
+                                </tr>
+                            </thead>
+                            <tbody id="tableBody">
+                                {userData.map(user => (
+                                    <tr key={user.id} >
+                                        <td className="email">{user.email}</td>
+                                        <td className="userName">{user.username}</td>
+                                        <td className="role">{user.roleId.rolename}</td>
+                                        <td >
+                                            <div className="row">
+                                                <div className="">
+                                                </div>
+                                                <div className="pr-3">
+                                                </div>
+                                            </div>
+                                            <button className="btn btn-danger" >Verwijderen </button>
+                                            {/* onClick={() => ToggleShowModalDelete(user)} */}
+                                            <button className="btn btn-moesland" >Aanpassen</button>
+                                            {/* onClick={() => ToggleShowModalUpdate(user)} */}
+                                        </td>
+                                    </tr>
+                                ))
+
+                                }
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+
+            }
+
+            {ShowAddEventModal && <ModalAdd toggleModal={ToggleShowAddEventModal} date={date} />}
         </>
     );
 
