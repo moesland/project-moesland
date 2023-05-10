40e6720ef019dc7557dd9914601262e55d7a2670
JackyZheng657 <j.zheng657@hotmail.com>
2023-05-10 12:41:06 +0200

    add async connect

--- backend/services/database.js
+++ backend/services/database.js
@@ -13,8 +13,8 @@ module.exports = async () => {
 
   if (process.env.NODE_ENV !== 'test' && mongoose.connection.readyState === 0) {
     const uri = process.env.MONGODB_URI;
-    console.log(uri);
-    mongoose.connect(uri).catch((err) => {
+    
+    await mongoose.connect(uri).catch((err) => {
       console.log(err);
     });
 
