ef544cefbbfb00a7a4d05d79aa85aae5172f6261
JackyZheng657 <j.zheng657@hotmail.com>
2023-05-18 06:38:19 +0200

    model added

---
+++ backend/models/participation.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const participationScema = new mongoose.Schema({
+    startnumber: {
+        type: Number,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    Category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'ParadeParticipationCategory',
+    },
+    Event: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Event',
+    },
+});
+
+module.exports = mongoose.model('participation', participationScema);
