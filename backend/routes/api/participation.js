ebe3d57aaca5c58d3a95d20afc10a57d1f085208
JackyZheng657 <j.zheng657@hotmail.com>
2023-05-18 07:32:53 +0200

    added body validation

--- backend/routes/api/participation.js
+++ backend/routes/api/participation.js
@@ -1,32 +1,53 @@
 const express = require('express');
+const { body, validationResult } = require('express-validator');
 const participationRepo = require('../../repository/paticipation');
 
 const router = express.Router();
 
 router.use(express.json());
 
-router.get('/', async (res) => {
+router.get('/', async (req, res) => {
   res.status(200).json(await participationRepo.getAll());
 });
 
-router.post('/', async (req, res, next) => {
+router.post('/', [
+  body('startnumber').isNumeric().notEmpty(),
+  body('name').notEmpty(),
+  body('Category').isMongoId().notEmpty(),
+  body('Event').isMongoId().notEmpty(),
+], async (req, res, next) => {
   try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
     const newParticipation = req.body;
     const createdParticipation = await participationRepo.add(newParticipation);
-    res.status(201).json(createdParticipation);
+    return res.status(201).json(createdParticipation);
   } catch (err) {
-    next(err);
+    return next(err);
   }
 });
 
-router.put('/:id', async (req, res, next) => {
+router.put('/:id', [
+  body('startnumber').optional().isNumeric(),
+  body('name').optional(),
+  body('Category').optional().isMongoId(),
+  body('Event').optional().isMongoId(),
+], async (req, res, next) => {
   try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
     const { id } = req.params;
     const newParticipation = req.body;
     const updatedParticipation = await participationRepo.update(id, newParticipation);
-    res.status(200).json(updatedParticipation);
+    return res.status(200).json(updatedParticipation);
   } catch (err) {
-    next(err);
+    return next(err);
   }
 });
 
