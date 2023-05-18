cec0082ddc4f17a9a93b2936a5e0c26bb68d1b7b
JackyZheng657 <j.zheng657@hotmail.com>
2023-05-18 07:59:13 +0200

    eslint fix

--- backend/repository/paticipation.js
+++ backend/repository/paticipation.js
@@ -2,39 +2,16 @@ const mongoose = require('mongoose');
 
 const Participation = mongoose.model('Participation');
 
-const getAll = async () => {
-  try {
-    return await Participation.find({});
-  } catch (err) {
-    throw err;
-  }
-};
+const getAll = async () => Participation.find({});
 
 const add = async (data) => {
-  try {
-    const participation = new Participation(data);
-    return await participation.save();
-  } catch (err) {
-    throw err;
-  }
+  const participation = new Participation(data);
+  return participation.save();
 };
 
-const remove = async (id) => {
-  try {
-    return await Participation.findByIdAndDelete(id);
-  } catch (err) {
-    throw err;
-  }
-};
+const remove = async (id) => Participation.findByIdAndDelete(id);
 
-const update = async (id, data) => {
-  try {
-    return await Participation.findByIdAndUpdate(id, data, { new: true });
-  } catch (err) {
-    throw err;
-  }
-};
-
+const update = async (id, data) => Participation.findByIdAndUpdate(id, data, { new: true });
 
 module.exports = {
   getAll,
