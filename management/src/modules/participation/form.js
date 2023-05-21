d15b0c3ad36536c02a2235233a274e3b33eea2d1
JackyZheng657 <j.zheng657@hotmail.com>
2023-05-21 05:43:33 +0200

    frontend validation added

--- management/src/modules/participation/form.js
+++ management/src/modules/participation/form.js
@@ -6,7 +6,8 @@ import { BackendFetch } from "../../services/ApiClient";
 const ModalForm = ({onClose, isUpdate = false, data = null}) => {
     const [eventOptions, seteventOptions] = useState([]);
     const [categorieOptions, setcategorieOptions] = useState([]);
-
+    const [errors, setErrors] = useState({});
+    
     useEffect(() => {
         BackendFetch('/api/event', 'GET', (eventData) => {
             seteventOptions(eventData);
@@ -19,15 +20,39 @@ const ModalForm = ({onClose, isUpdate = false, data = null}) => {
 
     const handleSubmit = async (event) => {
         event.preventDefault();
+
         const formData = new FormData(event.target);
         const formValues = Object.fromEntries(formData.entries());
 
-        const method = isUpdate ? 'PUT' : 'POST';
-        const endpoint = data ? `/api/participation/${data._id}` : '/api/participation';
+        if (validateForm(formValues)) {
+            const method = isUpdate ? 'PUT' : 'POST';
+            const endpoint = data ? `/api/participation/${data._id}` : '/api/participation';
+    
+            await BackendFetch(endpoint, method, (d) => {
+                onClose(true);
+            }, formValues);
+        }
+       
+    };
 
-        await BackendFetch(endpoint, method, (d) => {
-            onClose(true);
-        }, formValues);
+    const validateForm = (formValues) => {
+        const { name, startnumber} = formValues;
+        const newErrors = {};
+    
+        if (!name.trim()) {
+          newErrors.name = "Naam is required";
+        }
+    
+        if (!startnumber) {
+          newErrors.startnumber = "Startnummer is required";
+        } 
+        
+        if (startnumber <= 0) {
+          newErrors.startnumber = "Startnummer must be greater than 0";
+        }
+    
+        setErrors(newErrors);
+        return Object.keys(newErrors).length === 0;
     };
 
     return <CustomModal title={isUpdate ? "Deelname aanpassen" : "Deelnames toevoegen"} onClose={onClose}>
@@ -58,10 +83,12 @@ const ModalForm = ({onClose, isUpdate = false, data = null}) => {
                 <div className="form-group pt-3">
                     <label className="float-start">Naam</label>
                     <input className="form-control" placeholder="Naam" name="name" defaultValue={data ? data.name : ""}></input>
+                    {errors.name && <p className="text-danger mb-0">{errors.name}</p>}
                 </div>
                 <div className="form-group pt-3">
                     <label className="float-start">Startnummer</label>
                     <input className="form-control" placeholder="Startnummer" name="startnumber" type="number" defaultValue={data ? data.startnumber : ''}></input>
+                    {errors.startnumber && <p className="text-danger mb-0">{errors.startnumber}</p>}
                 </div>
             </div>
             <div className="modal-footer mt-3">
