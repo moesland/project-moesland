fe96004e1190ee003ada805b1dc7d4fcd717bcd9
JackyZheng657 <j.zheng657@hotmail.com>
2023-05-22 21:39:23 +0200

    Update management/src/modules/participation/overview.js
    Co-authored-by: Sven <70652307+Veaxx@users.noreply.github.com>

--- management/src/modules/participation/overview.js
+++ management/src/modules/participation/overview.js
@@ -24,7 +24,7 @@ const Overview = ({ toggleEditModal, toggleDeleteModal }) => {
                         <tr className="bg-moesland text-white">
                             <th scope="col">Event</th>
                             <th scope="col">Category</th>
-                            <th scope="col">deelname</th>
+                            <th scope="col">Deelname</th>
                             <th scope="col">Nr.</th>
                             <th scope="col">Acties</th>
                         </tr>
