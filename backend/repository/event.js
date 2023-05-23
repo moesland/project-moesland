06551edab70dbd67f75c214b989ec73c395a925d
JackyZheng657 <j.zheng657@hotmail.com>
2023-05-23 21:50:44 +0200

    eslint fix

--- backend/repository/event.js
+++ backend/repository/event.js
@@ -30,8 +30,17 @@ module.exports = {
       startdate: { $gte: start, $lt: end },
     }).catch((err) => console.error(err));
   },
-  async createEvent(title, description, startdate, enddate, location,
-    isParade, latitude, longitude, radius) {
+  async createEvent(
+    title,
+    description,
+    startdate,
+    enddate,
+    location,
+    isParade,
+    latitude,
+    longitude,
+    radius,
+  ) {
     const newEvent = new Event({
       title,
       description,
@@ -45,8 +54,18 @@ module.exports = {
     });
     await newEvent.save();
   },
-  async updateEventById(id, title, description, startdate, enddate, location,
-    isParade, latitude, longitude, radius) {
+  async updateEventById(
+    id,
+    title,
+    description,
+    startdate,
+    enddate,
+    location,
+    isParade,
+    latitude,
+    longitude,
+    radius,
+  ) {
     const cleanTitle = sanitize(title);
     const cleanDescription = sanitize(description);
     const cleanStartDate = sanitize(startdate);
