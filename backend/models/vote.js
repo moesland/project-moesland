679aef8e42a2cfd701a6839820b7806c954ad0f2
JackyZheng657 <j.zheng657@hotmail.com>
2023-05-24 18:55:14 +0200

    created model

---
+++ backend/models/vote.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const voteSchema = mongoose.Schema({
+    participant: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Participation',
+    },
+    category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'ParticipationCategory',
+    },
+    deviceId: {
+        type: String,
+        required: true,
+    },
+    timestamp: {
+        type: Date,
+        default: Date.now,
+    }
+});
+
+voteSchema.index({ category: 1, deviceId: 1 }, { unique: true });
+
+module.exports = mongoose.model('Vote', voteSchema);
