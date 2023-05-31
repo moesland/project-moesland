bec145450bba6de00c6bef10c8dfd291a71961e9
JackyZheng657 <j.zheng657@hotmail.com>
2023-05-31 22:39:21 +0200

    Created multi purpose api client

---
+++ moesland-app/src/services/MoeslandApi.jsx
@@ -0,0 +1,19 @@
+export const BackendFetch = async (path, method, callback, body = null) => {
+    try {
+        const response = await fetch(process.env.BACKEND_URL + path, {
+            method: method,
+            body: body ? JSON.stringify(body) : undefined
+        });
+        
+        console.log(response)
+
+        if (!response.ok) {
+            throw new Error(`Request failed with status ${response.status}`);
+        }
+        
+        const data = await response.json();
+        callback(data);
+    } catch (error) {
+        console.error('Error fetching:', error);
+    }
+};
