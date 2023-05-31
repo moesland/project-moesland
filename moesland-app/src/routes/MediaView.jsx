1dffc505104804b41ef214f17e54ac65f91df61f
BjornVanBruchem <b.vanbruchem1@student.avans.nl>
2023-05-31 20:42:10 +0200

    Start on the albums page

--- moesland-app/src/routes/MediaView.jsx
+++ moesland-app/src/routes/MediaView.jsx
@@ -1,25 +1,46 @@
-import React, { useEffect, useState } from 'react';
-import { Text, View } from 'react-native';
+import React, { useCallback, useEffect, useState } from 'react';
+import { FlatList, RefreshControl, Text, View } from 'react-native';
 import PhotoContent from './PhotoContentView';
 import { fetchAlbums } from '../services/FlickrApi';
 
 export default MediaView = () => {
   const [albums, setAlbums] = useState([]);
+  const [refreshing, setRefreshing] = useState(false);
 
   useEffect(() => {
-    const getAlbums = async () => {
-      const albums = await fetchAlbums();
-      setAlbums(albums);
-    };
-
     getAlbums();
   }, []);
 
+  const getAlbums = async () => {
+    const albums = await fetchAlbums();
+    setAlbums(albums);
+  };
+
+  const onRefresh = useCallback(async () => {
+    setRefreshing(true);
+    await getAlbums();
+    setRefreshing(false);
+  });
+
+  const renderItem = useCallback(({ item }) => {
+    return (
+      <View>
+        {console.log(item.id)}
+        <Text>{item.id}</Text>
+      </View>
+    );
+  });
+
   return (
-    <View>
-      {albums.map(album => {
-        <Text key={album.id}>{album.id}</Text>
-      })}
+    <View style={{ flex: 1 }}>
+      <FlatList
+        data={albums}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={renderItem}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+      />
 
       <PhotoContent />
     </View>
