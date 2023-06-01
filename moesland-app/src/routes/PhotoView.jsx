204369d384886fb26aa626ef0d18cb076278dd45
BjornVanBruchem <b.vanbruchem1@student.avans.nl>
2023-06-01 11:47:13 +0200

    Fix indents for lint

--- moesland-app/src/routes/PhotoView.jsx
+++ moesland-app/src/routes/PhotoView.jsx
@@ -2,11 +2,11 @@ import { Image, View } from "react-native";
 import styles from "../styles/PhotoViewStyles";
 
 export default PhotoView = (props) => {
-    const { imageSrc } = props.route.params;
+  const { imageSrc } = props.route.params;
 
-    return (
-        <View style={styles.container}>
-            <Image source={{ uri: imageSrc }} style={styles.photo} />
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Image source={{ uri: imageSrc }} style={styles.photo} />
+    </View>
+  );
 };
--- moesland-app/src/services/FlickrApi.jsx
+++ moesland-app/src/services/FlickrApi.jsx
@@ -2,41 +2,41 @@ const apiKey = process.env.FLICKR_API_KEY;
 const userId = '139654880@N02';
 
 const fetchAlbums = async () => {
-    const apiUrl = `https://api.flickr.com/services/rest/?method=flickr.photosets.getList&api_key=${apiKey}&user_id=${userId}&format=json&nojsoncallback=1`;
+  const apiUrl = `https://api.flickr.com/services/rest/?method=flickr.photosets.getList&api_key=${apiKey}&user_id=${userId}&format=json&nojsoncallback=1`;
 
-    try {
-        const response = await fetch(apiUrl);
-        const result = await response.json();
-        if (result.stat === 'ok') {
-            const albums = result.photosets.photoset;
-            return albums;
-        } else {
-            console.error('Error:', result.message);
-            return [];
-        }
-    } catch (err) {
-        console.error(err);
-        return [];
+  try {
+    const response = await fetch(apiUrl);
+    const result = await response.json();
+    if (result.stat === 'ok') {
+      const albums = result.photosets.photoset;
+      return albums;
+    } else {
+      console.error('Error:', result.message);
+      return [];
     }
+  } catch (err) {
+    console.error(err);
+    return [];
+  }
 };
 
 const fetchPhotosForAlbum = async (albumId) => {
-    const apiUrl = `https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=${apiKey}&photoset_id=${albumId}&format=json&nojsoncallback=1`;
+  const apiUrl = `https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=${apiKey}&photoset_id=${albumId}&format=json&nojsoncallback=1`;
 
-    try {
-        const response = await fetch(apiUrl);
-        const result = await response.json();
-        if (result.stat === 'ok') {
-            const photos = result.photoset.photo;
-            return photos;
-        } else {
-            console.error('Error:', result.message);
-            return [];
-        }
-    } catch (err) {
-        console.error(err);
-        return [];
+  try {
+    const response = await fetch(apiUrl);
+    const result = await response.json();
+    if (result.stat === 'ok') {
+      const photos = result.photoset.photo;
+      return photos;
+    } else {
+      console.error('Error:', result.message);
+      return [];
     }
+  } catch (err) {
+    console.error(err);
+    return [];
+  }
 };
 
 export { fetchAlbums, fetchPhotosForAlbum };
