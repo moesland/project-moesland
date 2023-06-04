5f0b87d526a01d9e846790d8f68d40a32ab02e34
JackyZheng657 <j.zheng657@hotmail.com>
2023-06-04 22:26:46 +0200

    LinearGradient added

--- moesland-app/src/modules/voting/VotingCategoryList.jsx
+++ moesland-app/src/modules/voting/VotingCategoryList.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { TouchableOpacity, Text, View, ScrollView, FlatList, Pressable } from 'react-native';
 import styles from '../../styles/votingStyles';
 import VotingItem from './VotingItems';
+import { LinearGradient } from 'expo-linear-gradient';
 
 const VotingCategoryList = ({data, votes, setVotes}) => {
     const [display, setDisplay] = useState(false);
@@ -12,7 +13,8 @@ const VotingCategoryList = ({data, votes, setVotes}) => {
 
     return (
         <ScrollView>
-            <TouchableOpacity style={[styles.categoryContainer, {backgroundColor: `${data.color}95`}]} onPress={toggleList}>
+            <TouchableOpacity style={[styles.categoryContainer]} onPress={toggleList}>
+                <LinearGradient colors={[`${data.color}`, `${data.color}75`, `${data.color}50`]} style={styles.background}/>
                 <Text style={styles.categoryTitle}>{data.name}</Text>
             </TouchableOpacity>
 
--- moesland-app/src/styles/votingStyles.js
+++ moesland-app/src/styles/votingStyles.js
@@ -61,7 +61,15 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         textAlign: 'center',
         color: 'gray',
-    }
+    },
+    background: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0,
+        borderRadius: 5,
+    },
 });
 
 export default styles;
